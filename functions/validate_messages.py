603e16dc59f8be0467e526d66c6aaff497ce8d68
KranthiRayipudi <Kranthi.Rayipudi.we@planninginspectorate.gov.uk>
2026-01-24 00:49:47 +0000

    changed validate_massage.py

--- functions/validate_messages.py
+++ functions/validate_messages.py
@@ -8,6 +8,12 @@ check that the date-time is compliant with ISO8601.
 
 from jsonschema import validate, FormatChecker, ValidationError
 from iso8601 import parse_date, ParseError
+import logging
+import json
+import azure.functions as func
+from typing import List, Dict, Any
+
+
 
 
 def is_iso8601_date_time(instance) -> bool:
@@ -40,3 +46,24 @@ def validate_data(message, schema: dict) -> bool:
         error_path = "/".join([str(p) for p in e.path]) or "<root>"
         errors = [f"{error_path}: {e.message}"]
     return errors
+
+def is_iso8601_date_time_trigger(instance) -> bool:
+    if instance is None or not isinstance(instance, str):
+        return False
+    try:
+        parse_date(instance)
+        return True
+    except ParseError:
+        return False
+
+def validate_data_trigger(message: Dict[str, Any], schema: dict) -> list:
+    """Returns list of errors (empty = valid)."""
+    format_checker = FormatChecker()
+    format_checker.checks("date-time")(is_iso8601_date_time_trigger)
+    
+    try:
+        validate(instance=message, schema=schema, format_checker=format_checker)
+        return []
+    except ValidationError as e:
+        error_path = "/".join([str(p) for p in e.path]) or "<root>"
+        return [f"{error_path}: {e.message}"]
